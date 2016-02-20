b4bab80c814793b7e44686ea6735b830ad4eea85
Seydou Dia <sdia.pyc@gmail.com>
2016-02-21 00:31:29 +0100

    add dimension_entropy_estimation.py

--- dimension_entropy_estimation.py
+++ dimension_entropy_estimation.py
@@ -39,37 +39,160 @@ some of the literature cited there as well.
 '''
 
 
-
-def d2():
-    "Correlation integral"
-    raise NotImplementError
+from multivariate import d2
 
 
 def av_d2():
-    "Correlation integral"
+
+    '''
+    Correlation integral
+
+    This program takes one of the output files of the d2, c1, or
+    c2naive programs as its input and smoothes the content of the file
+    by arithmetically averaging over a given interval. Furthermore, it
+    is possible to specify a range of embedding dimensions to be
+    printed. In the case of d2 output, the application of av-d2 makes
+    most sense on the local slopes file (extension .d2).
+
+
+    Suppose the input file contained values ri and di, then the output
+    will be in two columns
+
+
+
+                      __ a
+                 1    \
+        r    ,  ----   |       d
+         i      2a+1  /__ j=-a  i-j
+
+
+    where a is the argument to the -a option. We recommend to
+    experiment with the parameter a for optimal results.
+
+
+    Usage
+
+    Option    Description                    Default
+    -------------------------------------------------------
+    -m#       minimal dimension to print     1
+    -M#       maximal dimension to print     whole file
+
+    -a#       smooth over an interval of     1
+              2#+1 points
+
+    -E        show the length scales in      units of the data
+              rescaled units [0:1]
+    -------------------------------------------------------
+
+
+    Output
+
+    The output is the same as the one from the d2 and c2naive
+    programs. The only difference is that the output is smoothed.
+
+    Other post-processing for correlation sum data is available: c2d
+    computes local slopes from correlation sum data (extension .c2) by
+    fitting straight lines. This is essentially redundant with the
+    present program. Takens' estimator can be obtained using c2t while
+    c2g provides Gaussian kernel correlation integrals. Both act on
+    the correlation sum data (extension .c2).
+
+    '''
+
     raise NotImplementError
 
 
 def c2_naive():
-    "Correlation integral"
+
+    '''
+    Correlation integral
+
+
+    Usage
+
+    -d delay 
+    -M maximal embedding dimension 
+    -t minimal time separation 
+    -m minimal embedding dimension (1) 
+    -# resolution, values per octave (2) 
+    -T for Guido 
+    -l number of values to be read (all) 
+    -x number of values to be skipped (0) 
+    -c column to be read (1 or file,#) 
+
+
+    Determines the correlation sums of scalar time series in
+    file(s). A file named file_c2 is produced containing all the
+    embedding dimensions seperated by blank lines. No fast neighbour
+    search is applied but all available pairs are evaluated.
+
+    Note: You will probably use one of the auxilliary programs c2d,
+    c2t, or c2g to process the output further.
+    
+    Note: For longer signals, the program is much slower than the
+    version d2 that uses fast neighbour search.
+
+    '''
+
     raise NotImplementError
 
 
 def c2d():
-    "Correlation integral"
+
+    '''
+    Correlation integral
+
+    Local slopes from correlation sums
+
+
+    Usage
+
+    -o output file name, just -o means file_d 
+    -a average using -#,...,+# (1) 
+    -V verbosity level (0 = only fatal errors) 
+    -h show this message
+
+    Reads two columns, r(i), c(i) from file (correlation integral
+    output of c2naive, c1, or d2) and computes local slopes by fitting
+    straight lines. The range of the fits is specified by the option
+    -a . For the local slope at abscissa r(i), the values c(i-a), ...,
+    c(i+a) are used, where a is the argument to the -a option.
+
+    Alternatives to local slopes are Takens' estimator c2t or to use
+    Gaussian kernel smoothing c2g before computing slopes.
+
+    '''
+
     raise NotImplementError
 
 
 def c2t():
-    "Takens estimator"
+
+    '''
+    Takens estimator
+
+    Maximum likelihood estimator from correlation sums
+
+    Reads two columns, r, c(r) from file (correlation integral output
+    of c2naive, c1, or d2) and computes a maximum likelihood estimator
+    (Takens' estimator)
+
+                    C(r)
+         D (r) = ------------
+          T       /r    C(x)
+                 |  dx ------
+                 /0      x
+
+
+    as a function of the upper cutoff length scale. The integral is
+    computed from the discrete values of C(r) by assuming an exact
+    power law between the available points. Output is to stdout, or to
+    file file_t if -o is given.
+
+    '''
+
     raise NotImplementedError
 
+from multivariate import c1
 
-def c1():
-    "Fixed mass estimation of D1"
-    raise NotImplementedError
-
-
-def box_count():
-    "Renyi Entropies of Qth order"
-    raise NotImplementedError
+from multivariate import box_count
